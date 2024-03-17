e9cd8dd24bb15f7556e5be5048244ee53d784b16
Mary <mari.front.74@gmail.com>
2024-03-17 15:29:04 +0500

    🛠Bugfix: Tags were sent incorrectly when sending a note (solve)

--- src/app/components/reminders/remind-add/remind-add.component.ts
+++ src/app/components/reminders/remind-add/remind-add.component.ts
@@ -70,10 +70,15 @@ export class RemindAddComponent {
       return
     }
 
+    // Преобразование идентификаторов тегов в объекты тегов
+    const tagIds = this.f.tags.value;
+    const tagObjects = tagIds.map((id:number) => this.tags.find(tag => tag.id === id));
+
     const remind: Remind = {
       id: 0,
       title: this.f.title.value,
-      tags: this.f.tags.value ? this.f.tags.value.split(',') : [],
+/*      tags: this.f.tags.value ? this.f.tags.value.split(',') : [],*/
+      tags: tagObjects,
       deadline: this.f.deadline.value,
       remindMe: this.f.remindMe.value,
     };
